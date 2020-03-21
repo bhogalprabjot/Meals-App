c5ad220f9797e7b6572c55ddcfa0dd7fd51ac54d
bhogalprabjot <bhogalprabjot1@gmail.com>
2020-03-21 08:40:42 +0530

    First Navigation added

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 1.5,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ), // 3:2 ratio
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Easy Meals!'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                title: catData.title,
+                color: catData.color,
+                id: catData.id,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ), // 3:2 ratio
+      ),
     );
   }
 }
